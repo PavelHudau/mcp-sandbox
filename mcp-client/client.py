6142430f591eed707458330a1655d6adf602efbd
Pavel Hudau <paylikn@gmail.com>
2025-11-09 21:23:38 -0500

    client initial

---
+++ mcp-client/client.py
@@ -0,0 +1,33 @@
+from mcp import ClientSession, StdioServerParameters, types
+from mcp.client.stdio import stdio_client
+import asyncio
+import traceback
+
+serve_params = StdioServerParameters(
+    command="Uv",
+    args=["run", "weather.py"],
+)
+
+async def run():
+    try:
+        async with stdio_client(serve_params) as (reader, writer):
+            print("Client connected to server.")
+            async with ClientSession(reader, writer) as session:
+                print("Initializing session...")
+                await session.initialize()
+
+                print("Listing available tools...")
+                tools = await session.list_tools()
+                print("Available tools:", tools)
+
+
+                location = "New York"
+                result = await session.call_tool("get_weather", arguments={"location": location})
+                print(f"Weather in {location}: {result}")
+    except Exception as e:
+        print("An error occurred:")
+        traceback.print_exc()  
+
+
+if __name__ == "__main__":
+    asyncio.run(run())

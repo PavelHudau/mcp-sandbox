50b5f2a7472d7bddf832a4e37bd3e23e9d528850
Pavel Hudau <paylikn@gmail.com>
2025-11-28 21:09:54 -0500

    resources basic impl

---
+++ mcp-server/resources.py
@@ -0,0 +1,63 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("lResources")
+
+inventory_id_to_price = {
+    "item_001": 19.99,
+    "item_002": 29.99,
+    "item_003": 9.99,
+    "item_004": 49.99,
+}
+
+inventory_name_to_id = {
+    "Item One": "item_001",
+    "Item Two": "item_002",
+    "Item Three": "item_003",
+    "Item Four": "item_004",
+}
+
+
+@mcp.resource("inventory://overview")
+def get_inventory_overview() -> str:
+    """
+    Provides an overview of the current inventory items.
+    """
+    overview = """
+        Some inventory items:
+        - Item 1: Description of item 1
+        - Item 2: Description of item 2
+        - Item 3: Description of item 3 s
+        """
+    return overview.strip()
+
+
+@mcp.resource("inventory://{inventory_id}/price")
+def get_inventory_price_by_id(inventory_id: str) -> float:
+    """
+    Retrieves the price of an inventory item by its ID.
+
+    Args:
+        item_id (str): The ID of the inventory item.
+
+    Returns:
+        float: The price of the inventory item.
+    """
+    return inventory_id_to_price.get(inventory_id, 0.0)
+
+
+@mcp.resource("inventory://{inventory_name}/id")
+def get_inventory_id_by_name(inventory_name: str) -> str:
+    """
+    Retrieves the ID of an inventory item by its name.
+
+    Args:
+        item_name (str): The name of the inventory item.
+
+    Returns:
+        str: The ID of the inventory item.
+    """
+    return inventory_name_to_id.get(inventory_name, "")
+
+
+if __name__ == "__main__":
+    mcp.run()
